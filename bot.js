c9c968ae1b145ee101dc2f8e2d7bb1bb1a213680
Shadow <william.shadow@outlook.com>
2021-01-08 23:44:22 +0000

    fix old bugs

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ re.client.on('ready', () => console.log('\nStarting bot...'));
 if(re.config.debug.client) re.client.on('debug', m => console.debug(m));
 re.client.on('warn', m => console.log(m));
 re.client.on('error', m => console.error(m));
-re.client.on('botlog', m => re.logs.send(m))
+//re.client.on('botlog', m => re.config.logs.send(m))
 process.on('uncaughtException', error => console.error(error));
 re.client.commands = new re.vars.Discord.Collection()
 re.client.login(process.env.TOKEN);
