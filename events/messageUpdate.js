229e0226014558b9c20ddb681da63e9bb6996b08
Shadow <william.shadow@outlook.com>
2021-01-22 23:18:01 +0000

    updates for launch

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -3,3 +3,5 @@ const re = require(`../resources.js`).data
 re.client.on("messageUpdate", (oldmsg, newmsg) => {
   if(oldmsg.content != newmsg.content) re.client.emit("message", newmsg)
 });
+
+// This will allow edited messages to be triggered as commands also
