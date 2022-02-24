9f7b208d21342d9328cb09324db36144613b2b82
BipolarAnt <77637397+BipolarAnt@users.noreply.github.com>
2022-02-23 22:59:35 -0500

    Update wyr.js

--- config/wyr.js
+++ config/wyr.js
@@ -4,4 +4,12 @@ module.exports = [
   ["Be a Jedi", "Be a Sith"],
   ["Eat pie", "Eat cake"],
   ["Spam in the Cantina", "Make a thread like civilised beings"],
+  ["Be a Clone Trooper", "Be a Storm Trooper"],
+  ["Be a part of the Republic", "Be a part of the Separatist Alliance"],
+  ["Be Cad Bane", "Be Boba Fett"],
+  ["Be Hondo Onaka", "Be Jack Sparrow"],
+  ["Live in a treehouse", "Live in a cave"],
+  ["Be loved", "Be feared"],
+  ["Send a :carrot: for xp", "Send a :wolf: for xp"],
+  
 ]
