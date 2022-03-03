c7ff42bb8505f52d662263b0f41e78704e00628d
BipolarAnt <77637397+BipolarAnt@users.noreply.github.com>
2022-03-02 23:21:23 -0500

    Update wyr.js

--- config/wyr.js
+++ config/wyr.js
@@ -11,5 +11,23 @@ module.exports = [
   ["Live in a treehouse", "Live in a cave"],
   ["Be loved", "Be feared"],
   ["Send a :carrot: for xp", "Send a :wolf: for xp"],
+  ["Be a Jedi", "Be a Mandolorian"],
+  ["Eat a pizza", "Eat some tacos"],
+  ["Have the ability to fly", "Have the ability to rewind time"],
+  ["Marvel Comics", "DC Comics"],
+  ["Marvel Heroes", "DC Heroes"],
+  ["Marvel Movies", "DC Movies"],
+  ["Only be able to speak like yoda", "Only be able to speak in a foreign language"],
+  ["Thrawn", "Plo Koon"],
+  ["Read fiction", "Read non-fiction"],
+  ["Pirate :potato:", "Potato :pirate_flag:"],
+  ["Join the Avengers", "Join the Justice League"],
+  ["Star Wars: The Clone Wars", "Star Wars: Rebels"],
+  ["Star Wars Prequel Trilogy", "Star Wars Sequel Trilogy"],
+  ["Americans", "Brits"],
+  ["Time Travel to meet the 1st President", "Time Travel to meet the 100th president"],
+  ["Fantasy football", "Football in fantasy"],
+  ["Star Wars Legends", "Star Wars Canon"],
+  
   
 ]
