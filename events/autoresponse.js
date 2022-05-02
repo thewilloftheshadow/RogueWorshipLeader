c6877c76dd0fde8007d03d540fdaa6d540afc41a
Shadow <william.shadow@outlook.com>
2022-05-01 19:10:52 -0500

    fix autoresponse for multiple words

--- events/autoresponse.js
+++ events/autoresponse.js
@@ -5,23 +5,14 @@ module.exports = (client) => {
     if (message.author?.bot) return
     if (!message.guild?.id) return
     let check = message.content.toLowerCase()
-    let triggers = check.split(" ")
+    
+    let responses = await autoresponse.find({guild: message.guild.id, deleted: false})
+    responses.forEach(x => {
+      if(check.includes(x.trigger)) {
+        await channel.send({ content: `${response}` })
+      }
+    })
 
     triggers.forEach((x) => respond(x, message.channel))
   })
-}
-
-const respond = async (key, channel) => {
-  if (process.env.DEBUG) console.log(`autoresponse: ${key}`)
-  let responseDb = await autoresponse.findOne({ trigger: key, guild: channel.guild.id, deleted: false })
-
-  let response = responseDb?.response
-
-  if (!response) return
-
-  let m = await channel.send({ content: `${response}` })
-}
-
-const sleep = async (time) => {
-  await new Promise((r) => setTimeout(r, time))
 }
