1748548ea205b5726fe84a6aeaffdd167a80a284
StarLance1100 <mcain.edu@gmail.com>
2023-02-13 02:20:14 -0600

    Update do.js
    first update to the do command

--- config/do.js
+++ config/do.js
@@ -1,5 +1,63 @@
-const do = {
+const gifs = {
+  "hug": [
+    "https://giphy.com/gifs/CMNHospitals-9xrrQ9UX8ImQWlHGwv",
+    "https://giphy.com/gifs/theoffice-the-office-tv-business-ethics-l8ooOxhcItowwLPuZn",
+    "https://giphy.com/gifs/love-disney-girl-EvYHHSntaIl5m",
+    "https://giphy.com/gifs/animation-cute-illustration-Bj9k1U69GZ8Iw",
+    "https://giphy.com/gifs/hug-hugging-gnXG2hODaCOru",
+    "https://giphy.com/gifs/starwars-star-wars-tv-show-rebels-l2JJySFVazmR38Lks",
+  ],
+
+  "high five": [
+    "https://giphy.com/gifs/brooklynninenine-nbc-brooklyn-nine-99-s4VoCsFz8prlhSFCeS",
+    "https://giphy.com/gifs/filmeditor-high-five-sacha-baron-cohen-l0ErFafpUCQTQFMSk",
+    "https://giphy.com/gifs/90s-high-five-6yKquSnGwI5Ak",
+    "https://giphy.com/gifs/tmnt-nickelodeon-teenage-mutant-ninja-turtles-animatiom-ZmlhrJxcvyNxu",
+    "https://giphy.com/gifs/disney-aladdin-genie-magic-carpet-T5MvalD01r2XS",
+
+  ],
+
+  "slap": [
+    "https://giphy.com/gifs/slap-dog-slapping-lX03hULhgCYQ8",
+    "https://giphy.com/gifs/moodman-batman-smack-smacked-Qumf2QovTD4QxHPjy5",
+    "https://giphy.com/gifs/pokemon-slap-jigglypuff-w5FSoU86sXRFm",
+    "https://giphy.com/gifs/siblings-retriever-golden-retrievers-LSc9OoFCKcVlLYYb3d",
+    "https://giphy.com/gifs/starwars-movie-star-wars-l3fZKbEJfXuF8Z9mg",
+  ],
+
+
+
+  "fist bump": [
+    "https://giphy.com/gifs/alaskaairlines-yup-prepared-alaska-air-PlgCsMUYAC2kFC5GSL",
+    "https://tenor.com/view/bros-the-office-love-fist-bump-gif-13830753",
+    "https://tenor.com/view/free-anime-rin-matsuoka-sousuke-gif-18204553",
+    "https://tenor.com/view/fist-bump-boom-pound-it-secret-hand-shake-deal-gif-10320174",
+    "https://tenor.com/view/fist-bump-big-hero6-gif-14779823",
+    "https://tenor.com/view/santa-clarita-diet-boom-fist-bump-gif-11981274",
+    "https://tenor.com/view/the-office-michael-scott-steve-carell-fist-bump-gif-4439458",
+    "https://tenor.com/view/fist-bump-otter-gif-10989261",
+    "https://tenor.com/view/fist-bump-fist-e22-friends-gif-15544967",
+    "https://tenor.com/view/bro-handshake-high-five-fist-bump-gif-13891059",
+    "https://tenor.com/view/sheldon-fist-bump-shake-big-bang-theory-gif-15770421",
+  ],
+
+  "hit": [
+    "https://giphy.com/gifs/afv-soccer-hit-inflatable-xTiTnzwJoXpg7gdONy",
+    "https://giphy.com/gifs/head-hit-shovel-43bOrDOasXG6Y",
+    "https://giphy.com/gifs/guy-qwRhCIj6X2MeY",
+    "https://giphy.com/gifs/disney-tangled-the-emperors-new-groove-kuzco-xjHj7TPdbCN8I",
+    "https://tenor.com/view/penguin-hit-head-smack-head-funny-fall-gif-16306739",
+    "https://tenor.com/view/hit-virtual-hit-sending-hit-sent-gif-13646565",
+    "https://tenor.com/view/sml-cody-textbook-throwing-throw-gif-22985679",
+    "https://tenor.com/view/moris-cipson-twoj-stary-twoja-stara-minecraft-fortnite-xd-beka-memy-memes-mem-call-of-duty-warzone-bedoes-gif-20247198",
+    "https://tenor.com/view/cat-cat-hit-hitting-hit-you-gif-26488911",
+    "https://tenor.com/view/cute-angry-cat-go-away-gif-25389837",
+    "https://images-ext-1.discordapp.net/external/WGjAffW2e8NcVlw3TLZfK9jY5LgC_hXu1GN8IB68nrA/https/media.tenor.com/K0u17jE1AaEAAAPo/minion-bonk.mp4",
+    
+
+
+  ],
+
 
 }
-
-module.exports = do
+module.exports = gifs
